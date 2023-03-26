b13512698f783cf42e690e46fedda7eeb6dea120
Edinor Junior <edinorscjr@gmail.com>
2023-03-26 16:55:21 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_ecr_repository" "app" {
+  name = "app"
+}
+
+resource "aws_ecs_task_definition" "app" {
+  family                   = "app"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+
+  container_definitions = jsonencode([{
+    name            = "app"
+    image           = "${aws_ecr_repository.app.repository_url}:latest"
+    portMappings    = [{
+      containerPort = 3000
+      hostPort      = 0
+    }]
+    essential       = true
+  }])
+
+  memory     = 512
+  cpu        = 256
+}
+
+resource "aws_ecs_service" "app" {
+  name            = "app"
+  cluster         = "app"
+  task_definition = "${aws_ecs_task_definition.app.arn}"
+  desired_count   = 1
+
+  network_configuration {
+    subnets          = ["subnet-12345678"]
+    security_groups  = ["sg-12345678"]
+    assign_public_ip = "true"
+  }
+}
+
+output "url" {
+  value = "http://${aws_ecs_service.app.load_balancers[0].dns_name}:3000"
+}
